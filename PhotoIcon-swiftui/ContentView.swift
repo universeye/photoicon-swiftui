f938411d6193643fceddcb6abe9af86d5782e501
Terry <terrykuo501@gmail.com>
2021-06-21 12:08:41 +0800

    Initial Commit

---
+++ PhotoIcon-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoIcon-swiftui
+//
+//  Created by Terry Kuo on 2021/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoIcon-swiftui/PhotoIcon_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoIcon_swiftuiApp.swift
+//  PhotoIcon-swiftui
+//
+//  Created by Terry Kuo on 2021/6/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoIcon_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
