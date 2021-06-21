c0a3eb76f1e35bcaa04afff2ede6691ae5c8f0f7
Terry <terrykuo501@gmail.com>
2021-06-21 12:25:13 +0800

    Initial commit

--- PhotoIcon-swiftui/ContentView.swift
+++ PhotoIcon-swiftui/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+            Photoicon()
+                .offset(x: 0, y: -39)
+                .frame(width: 300, height: 300)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .shadow(color: .black, radius: 10, x: 0.0, y: 0.0)
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.sizeThatFits)
+    }
+}
+
+
+struct Photoicon: View {
+    var body: some View {
+        ZStack {
+            ForEach(0..<8) { item in
+                RoundedRectangle(cornerRadius: 27)
+                    .frame(width: 48, height: 80)
+                    .foregroundColor(.orange)
+                    .hueRotation(.degrees(Double(item) * 45))
+                    .blendMode(.multiply)
+                    //.offset(y: -5)
+                    .rotationEffect(.degrees(Double(item) * 45), anchor: .bottom)
+            }
+        }
     }
 }
